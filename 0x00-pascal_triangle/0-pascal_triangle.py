d3f76202cd510e5a1d6d4ba7f16b813a4973f2ed
FionaG26 <fionamuthoni18@gmail.com>
2023-06-27 07:17:41 -0700

    pascal

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Pascal triangle
+"""
+
+def pascal_triangle(n):
+    """
+    Generate Pascal's triangle of size n.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
+
