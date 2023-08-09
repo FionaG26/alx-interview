48f8554f6f89598b9a60266e2094451ca14c411d
FionaG26 <fionamuthoni18@gmail.com>
2023-08-09 05:54:00 -0700

    jedi

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+request(apiUrl, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const filmData = JSON.parse(body);
+    const characters = filmData.characters;
+
+    characters.forEach((characterUrl) => {
+      request(characterUrl, (charError, charResponse, charBody) => {
+        if (!charError && charResponse.statusCode === 200) {
+          const characterData = JSON.parse(charBody);
+          console.log(characterData.name);
+        }
+      });
+    });
+  }
+});
