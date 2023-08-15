676fad1fa2755930fd395a380949151cfcb1990b
FionaG26 <fionamuthoni18@gmail.com>
2023-08-15 02:00:44 -0700

    matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,6 +3,7 @@
 Rotate 2D Matrix
 """
 
+
 def rotate_2d_matrix(matrix):
     """
     Rotates a 2D matrix 90 degrees clockwise.
@@ -19,6 +20,7 @@ def rotate_2d_matrix(matrix):
     # Reverse each row
     for i in range(n):
         matrix[i] = matrix[i][::-1]
+
 
 if __name__ == "__main__":
     matrix = [[1, 2, 3],
